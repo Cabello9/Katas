193e404fd2f5d25de08896c3e6f39ba41ac528e8
Carlos Martinez <cmartinez@redvelgames.com>
2024-05-24 09:22:43 +0200

    test: ensure statements are printed in order

--- Katas/Bank.Tests/AcceptanceTests.cs
+++ Katas/Bank.Tests/AcceptanceTests.cs
@@ -51,9 +51,12 @@ public class AcceptanceTests
         sut.Deposit(2000);
         sut.Withdraw(500);
 
-        sut.Statement.Should().HaveCount(3);
-        sut.Statement[0].Should().Be("14/01/2012 || -500 || 2500");
-        sut.Statement[1].Should().Be("13/01/2012 || 2000 || 3000");
-        sut.Statement[2].Should().Be("10/01/2012 || 1000 || 1000");
+        Received.InOrder(() =>
+        {
+            sut.Statement.Should().HaveCount(3);
+            sut.Statement[0].Should().Be("14/01/2012 || -500 || 2500");
+            sut.Statement[1].Should().Be("13/01/2012 || 2000 || 3000");
+            sut.Statement[2].Should().Be("10/01/2012 || 1000 || 1000");
+        });
     }
 }
